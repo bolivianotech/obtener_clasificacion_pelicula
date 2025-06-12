a2be608de5f0b1578ab51066191275540e1a21db
Jim Nataniel <95052718+bolivianotech@users.noreply.github.com>
2025-06-12 12:05:29 +0000

    Añadiendo comida_favorita.py

---
+++ comida_favorita.py
@@ -0,0 +1,13 @@
+# Paso 1: Crear la lista
+comidas_favoritas = ['Pizza', 'Sushi', 'Tacos']
+# Paso 2: Imprimir la lista
+print(comidas_favoritas)
+# Paso 3: Imprimir el segundo elemento (índice 1)
+print(f"Mi segunda comida favorita es: {comidas_favoritas[1]}")
+# Paso 4: Modificar el primer elemento (índice 0)
+comidas_favoritas[0] = "Majadito"
+# Paso 5: Imprimir la lista modificada
+print(comidas_favoritas)
+# Paso 6: Imprimir la longitud d
+print(len(comidas_favoritas))
+print("Jimmy Requeña - FIN DEL PROGRAMA")
