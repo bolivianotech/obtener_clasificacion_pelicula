876239867b3f8323b07500555f812d7618800268
Jim Nataniel <95052718+bolivianotech@users.noreply.github.com>
2025-06-16 13:17:46 +0000

    añadi comidas.py para Marco Aurelio Barriga

---
+++ comidas.py
@@ -0,0 +1,11 @@
+#Hobbies 
+hobbies_M = ["Dibujar", "Jugar codsito", "Realizar Isp pinout"]
+lista_hobbies =  hobbies_M
+print(f"Estos son mis hobbies: {lista_hobbies}")
+print(f"Mi segundo hobbies es: {lista_hobbies[1]}")
+comidas_favoritas = ["Picante de pollo", "Pomllito frito", "Sopa de mani" ]
+print(f"Estas son mis comidas favoritas: {comidas_favoritas}")
+print(f"Mi segunda comida cambiara de: {comidas_favoritas[1]}")
+comidas_favoritas[1] = "Salteña"
+print(f"Mi segunda comida cambiara a: {comidas_favoritas[1]}")
+print(f"Ahora estas son mis comidas favoritas: {comidas_favoritas}")
